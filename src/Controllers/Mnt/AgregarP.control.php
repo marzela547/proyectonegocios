d7e65912d1a379ba385a6013a12353b3939e04c3
Franciscogz <65878436+Franciscogz@users.noreply.github.com>
2021-07-17 00:24:58 -0600

    Variables Controlador AgregarP

--- src/Controllers/Mnt/AgregarP.control.php
+++ src/Controllers/Mnt/AgregarP.control.php
@@ -5,6 +5,42 @@ namespace Controllers\Mnt;
 class AgregarP extends \Controllers\PublicController{
     public function run(): void
     {
+
+        $viewData = array();
+        $viewData["idPro"] ="";
+        $viewData["nombrePro"] ="";
+        $viewData["marcapro"] = "";
+        $viewData["categoriapro"] = "";
+        $viewData["preciopro"] = "";
+        $viewData["imgpro"] = "";
+        $viewData["descripcionpro"] = "";
+
+        if($this->isPostBack()){
+            $viewData["idPro"] = $_POST["idPro"];
+            $viewData["nombrePro"] = $_POST["nombrePro"];
+            $viewData["marcapro"] = $_POST["marcapro"];
+            $viewData["categoriapro"] = $_POST["categoriapro"];
+            $viewData["preciopro"] = $_POST["preciopro"];
+            $viewData["imgpro"] = $_POST["imgpro"];
+            $viewData["descripcionpro"] = $_POST["descripcionpro"];
+
+            switch($viewData["mode"])
+            {
+                case "INS":
+                    $ok = \Dao\ProductosPanel::addProductos(
+                        $viewData["nombrePro"],
+                        $viewData["marcapro"],
+                        $viewData["categoriapro"],
+                        $viewData["preciopro"],
+                        $viewData["imgpro"],
+                        $viewData["descripcionpro"]
+                    );
+                    break;
+            }
+        }else{
+            /*$viewData["mode"]== $_GET["mode"];*/
+            $viewData["idPro"]== isset($_GET["idPro"]);
+        }
         /*\Utilities\Site::addLink("public/css/agregarp.css");*/
         $Datos = array();
         \Views\Renderer::render("mnt/agregarp", $Datos);
